f7b2f62f37eb440bbcf9cb2d8c42c5eb241f31d9
Nemkac <nemanjatodorovic132002002@gmail.com>
2024-02-26 02:35:13 +0100

    React router implementation

--- frontend/src/Pages/HomePage.tsx
+++ frontend/src/Pages/HomePage.tsx
@@ -4,6 +4,7 @@ import axios, { AxiosResponse } from 'axios'
 import { url } from '../Endpoints'
 import EstablishmentCard from '../Components/EstablishmentCard'
 import { Establishment } from '../Models/Establishment'
+import SidePanel from '../Components/SidePanel'
 
 type Props = {}
 
@@ -30,16 +31,23 @@ const HomePage : React.FC<Props> = (props: Props) : JSX.Element => {
 
   return (
     <div className='flex flex-col w-full h-full'>
-        <div>
-          <Map/>
+      <div className='flex flex-row w-full h-full px-10 py-6 justify-between'>
+        <div className='flex w-[18%] h-full'>
+          <SidePanel/>
         </div>
-        <div className='flex flex-row w-full h-auto justify-between flex-wrap mt-10'>
-          {
-            establishments.map((establishment, index) => (
-              <EstablishmentCard key={index} id={establishment.id} name={establishment.name} description={establishment.description} type={establishment.type}/>
-            ))
-          }
+        <div className='flex flex-col w-[80%] h-full'>
+          <div>
+            <Map/>
+          </div>
+          <div className='flex flex-row w-full h-auto justify-between flex-wrap mt-10'>
+            {
+              establishments.map((establishment, index) => (
+                <EstablishmentCard key={index} id={establishment.id} name={establishment.name} description={establishment.description} type={establishment.type}/>
+              ))
+            }
+          </div>
         </div>
+      </div>
     </div>
   )
 }
---
+++ frontend/src/Pages/VisitEstablishmentPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const VisitEstablishmentPage : React.FC<Props> = (props: Props) : JSX.Element => {
+  return (
+    <div>VisitEstablishmentPage</div>
+  )
+}
+
+export default VisitEstablishmentPage
